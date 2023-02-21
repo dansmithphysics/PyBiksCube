0b4e35f623f90b79e345030b23da6d6d71074a96
Daniel Smith <danielsmith@uchicago.edu>
2023-02-20 19:46:58 -0600

    Running utilities through the linter

--- PyBiksCube/utilities.py
+++ PyBiksCube/utilities.py
@@ -1,4 +1,4 @@
-
+""" Module of utilities useful for interacting with the Cube """
 
 def side_type_converter(side, reverse=False):
     """
@@ -21,30 +21,27 @@ def side_type_converter(side, reverse=False):
         return [side_type_converter(side_, reverse) for side_ in side]
 
     legal_xyz = ['x', 'y', 'z', '-x', '-y', '-z']
-    legal_UFDLRB = list('UFDLRB')
+    legal_ufdlrb = list('UFDLRB')
 
-    if side not in legal_xyz + legal_UFDLRB:
-        raise ValueError("Cannot convert side value: %s" % side)
-    
+    if side not in legal_xyz + legal_ufdlrb:
+        raise ValueError("Cannot convert side value: {side}")
+
     if reverse:
         if side in legal_xyz:
             return side
-        else:
-            side_map = {'F': 'x', 'B':'-x',
-                        'R': 'y', 'L':'-y',
-                        'U': 'z', 'D':'-z'}
-            if side not in side_map:
-                raise ValueError
-            else:
-                return side_map[side]
-    else:        
-        if side in legal_UFDLRB:
+
+        side_map = {'F': 'x', 'B':'-x',
+                    'R': 'y', 'L':'-y',
+                    'U': 'z', 'D':'-z'}
+    else:
+        if side in legal_ufdlrb:
             return side
-        else:
-            side_map = {'x': 'F', '-x': 'B',                    
-                        'y': 'R', '-y': 'L',                    
-                        'z': 'U', '-z': 'D'}
-            if side not in side_map:
-                raise ValueError
-            else:
-                return side_map[side]
+
+        side_map = {'x': 'F', '-x': 'B',
+                    'y': 'R', '-y': 'L',                    
+                    'z': 'U', '-z': 'D'}
+
+    if side not in side_map:
+        raise ValueError("Side map converter does not have side: {side}")
+
+    return side_map[side]
