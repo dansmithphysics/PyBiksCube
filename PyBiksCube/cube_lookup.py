8a8c30e626be4f12d24b22f8d0910acdee011ca1
Daniel Smith <danielsmith@uchicago.edu>
2023-02-26 15:56:58 -0600

    Linter and docstrings

--- PyBiksCube/cube_lookup.py
+++ PyBiksCube/cube_lookup.py
@@ -8,7 +8,7 @@ from matplotlib.patches import Rectangle
 
 class CubeLookup:
     """
-    Representation of a Rubik's Cube. 
+    Representation of a Rubik's Cube.
     Uses a lookup table for moves, optimized for speed.
     Includes:
 
@@ -49,6 +49,9 @@ class CubeLookup:
 
             # Check if the default file exists. If not, create it.
             if not os.path.isfile(lookup_table_file_name):
+                # It is not typical to import functions mid-code.
+                # However, this is only used if the default table doesn't already exist
+                # and importing here solves a cyclical import error.
                 from PyBiksCube.create_lookup_table import create_lookup_table
                 create_lookup_table(lookup_table_file_name)
         else:
@@ -59,18 +62,64 @@ class CubeLookup:
         try:
             self.move_array = np.loadtxt(lookup_table_file_name, delimiter=",", dtype=np.int16)
         except:
-            ValueError("Something wrong happened with opening the lookup table.")
+            raise ValueError("Something wrong happened with opening the lookup table.")
 
     def set_cube_state(self, cube_state_):
+        """
+        Sets cube face colors based on the
+        cube_state, a 54 long string.
+
+        Parameters
+        ----------
+        cube_state : str
+            String of 54 characters for color of different faces.
+            Order matches self.cube_state_map
+        """
+
         self.cube_state = np.array(list(cube_state_), dtype=str)
 
     def get_cube_state(self):
+        """
+        Returns cube face colors based on a 54 long string.
+
+        Returns
+        -------
+        cube_state : str
+            String of 54 characters for color of different faces.
+            Order matches self.cube_state_map.
+        """
+
         return "".join(self.cube_state)
 
     def get_raw_cube_state(self):
+        """
+        Returns the raw cube face colors based on an array of 54 strings.
+
+        Returns
+        -------
+        cube_state : array of str
+            Array of 54 strings for color of different faces.
+            Order matches self.cube_state_map.
+        """
+
         return self.cube_state
 
     def move_decoder(self, move_command):
+        """ 
+        Decodes move command, decomposing more complicated moves
+        into their fundamental movement components that are
+        then executed.
+
+        The notation must be the corresponding integer for each move.
+        The mapping, as defined in create_lookup_table, is:
+        U:0, F:1, D:2, L:3, R:4, B:5, U':6, F':7, D':8, L':9, R':10, B':11
+
+        Parameters
+        ----------
+        move_command : str
+            Move command to perform.
+        """
+
         if isinstance(move_command, (list, np.ndarray)):
             for move_command_ in move_command:
                 self.move_decoder(move_command_)
@@ -85,9 +134,34 @@ class CubeLookup:
         self._fundamental_move(move_command)
 
     def _fundamental_move(self, move_command):
+        """ 
+        Executes fundamental movements.
+        The fundamental movements are U, D, R, L, F, B
+        and U', D', R', L', F', B'
+        The notation must be the corresponding integer for each move.
+        The mapping, as defined in create_lookup_table, is:
+        U:0, F:1, D:2, L:3, R:4, B:5, U':6, F':7, D':8, L':9, R':10, B':11
+
+        Parameters
+        ----------
+        move_command : str
+            Move command to perform.
+        """
+
         self.cube_state = self.cube_state[self.move_array[move_command]]
 
     def randomize(self, n_moves=None):
+        """
+        Randomizes the cube state by applying 
+        n_moves number of random moves on cube.
+
+        Parameters
+        ----------
+        n_moves : int
+            Number of random moves to move.
+            Default of None randomly selects an number from 1 to 30.
+        """
+
         if n_moves is None:
             n_moves = np.random.randint(1, 30)
         mc_moves = np.random.choice(np.arange(12, dtype=np.int16), n_moves)
@@ -95,12 +169,46 @@ class CubeLookup:
         return mc_moves
 
     def set_default_cube_state(self):
+        """
+        Sets cube state to default Rubik's cube given.
+        The red face on top and yellow face on front.
+        """
+
         self.set_cube_state("rrrrrrrrryyyyyyyyymmmmmmmmmgggggggggbbbbbbbbbwwwwwwwww")
 
     def check_solved(self):
+        """ 
+        Checks whether the cube is solved.
+        Perhaps this can be sped up by only looking at
+        the minimum needed, but looking at the full cube for now.
+
+        Returns
+        -------
+        solved : bool
+            Boolean of whether or not the cube is solved.
+        """
+
         return self.get_cube_state() == "rrrrrrrrryyyyyyyyymmmmmmmmmgggggggggbbbbbbbbbwwwwwwwww"
 
     def check_match_against_key(self, key):
+        """
+        Checks if the cube state matches the key (mask) given.
+        The key can have blank spots, designated with the letter 'k',
+        which are not used in the match calculation.
+
+        Function is used for the Solver class to find which
+        moves to apply to the cube based on a key (mask).
+
+        Parameters
+        ----------
+        key : array of str
+            Array of strings, length of 54, corresponding to the faces of the cube.
+
+        Returns
+        -------
+        match : bool
+            Boolean of whether or not the key matches the cube.
+        """
         converted_key = np.array(list(key), dtype=str)
         count_matches = np.sum(np.logical_and(converted_key == self.cube_state,
                                               converted_key != 'k'))
--- PyBiksCube/solver.py
+++ PyBiksCube/solver.py
@@ -1,13 +1,48 @@
+""" This module defines the Solver """
+
 import os.path
 import numpy as np
-from numpy import array, int16
-
+from numpy import array, int16 # Needed for eval on loaded file
 
 class Solver:
+    """
+    Solver class for the Rubik's Cube.
+
+    Algorithmically solves the cube in steps.
+    In each step, there is a dictionary with keys that
+    represent each state the cube can be during this stage
+    after being masked for the pieces being solved in that stage.
+    The key's corresponding values are lists of moves needed to
+    bring the cube in to the next stage.
+
+    The most straight-forward approach is to solve for each
+    piece iteratively, with 20 stages and each stage not having more
+    than 24 possible cube states. The default solver uses this approach.
+
+    Attributes
+    ----------
+    array_of_dict_solvers : array of dictionaries
+        Array of the dictionaries used in each stage. 
+    cube : Cube object being solved.
+    """
+
     def __init__(self, solver_file_name=None):
+        """
+        The constructor for the Solver class.
+
+        Parameters
+        ----------
+        solver_file_name : str
+            File name of where the array of dictionaries for solving the cube are saved.
+            If "default", attempts to load the default solver in data/default_algorithm_solver.txt.
+            Creates the default if doesn't exist.
+            If None, initializes a blank array.
+            Otherwise, attempts to load the designated solver file.
+        """
+
         self.array_of_dict_solvers = []
         self.cube = None
-        
+
         if solver_file_name is not None:
             if solver_file_name == "default":
                 solver_file_name = os.path.join(os.path.dirname(os.path.realpath(__file__)),
@@ -15,6 +50,9 @@ class Solver:
 
                 # Check if the default file exists. If not, create it.
                 if not os.path.isfile(solver_file_name):
+                    # It is not typical to import functions mid-code.
+                    # However, this is only used if the default solver doesn't already exist
+                    # and importing here solves a cyclical import error.
                     from PyBiksCube.create_solution_algorithm import create_algorithm
                     create_algorithm(solver_file_name)
             else:
@@ -29,8 +67,19 @@ class Solver:
                 raise ValueError("Something wrong happened with opening the algorithm file.")
 
     def solve_cube(self, cube, output_moves=False):
-        self.cube = cube
+        """
+        Solves the given cube using the algorithm that is already loaded into the class.
         
+        Parameters
+        ----------
+        cube : cube object
+            The cube to be solved. Is loaded into the class attribute cube.
+        output_moves : bool
+            Returns the moves used to solve. 
+        """
+
+        self.cube = cube
+
         if output_moves:
             total_moves_to_solve = np.array([], dtype=np.int16)
 
@@ -42,13 +91,27 @@ class Solver:
                 total_moves_to_solve = np.append(total_moves_to_solve, moves_to_solve)
 
         self.cube = None
-                
+
         if output_moves:
-            return total_moves_to_solve        
-        
+            return total_moves_to_solve
+
         return None
 
     def find_moves_to_solve_stage(self, i_solver_dict):
+        """
+        Helper function to find the moves to solve a single stage of the solver algorithm.
+
+        Parameters
+        ----------
+        i_solver_dict : int
+            Index of current stage being solved for.
+
+        Returns
+        -------
+        moves_to_solve : array of integers
+            The moves needed to solve this stage of the cube.
+        """
+
         solver_dict = self.array_of_dict_solvers[i_solver_dict]
         end_stage = self.cube.get_raw_cube_state()
 
